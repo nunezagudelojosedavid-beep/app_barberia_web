b156cc92da4b8e4e69bcaafd2a26b556c7172d93
nunezagudelojosedavid-beep <nunezagudelojosedavid@gmail.com>
2025-09-17 00:19:51 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app_barberia_web',
+  webDir: 'www'
+};
+
+export default config;
